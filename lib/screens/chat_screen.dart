e0b5d04364a7bec41edee152c07fd04fd27ff80f
IL KWON KIM <magictical@gmail.com>
2020-07-21 11:02:47 +0900

    add FloatingActionButton to fetch data from firestore

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
@@ -10,6 +11,19 @@ class ChatScreen extends StatelessWidget {
           child: Text('this is list item'),
           padding: EdgeInsets.all(8),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/DNX9EhYKqZQNftHJCg1L/messages')
+              .snapshots()
+              .listen((data) {
+            data.documents.forEach((document) {
+              print(document['text']);
+            });
+          });
+        },
       ),
     );
   }
