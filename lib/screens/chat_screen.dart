95e12f71aebe6192c91093740171125cf0a6d1fd
IL KWON KIM <magictical@gmail.com>
2020-07-21 11:23:11 +0900

    update to StreamBuilder to fetch data from firestore

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -5,25 +5,30 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          child: Text('this is list item'),
-          padding: EdgeInsets.all(8),
-        ),
-      ),
+      body: StreamBuilder(
+          stream: Firestore.instance
+              .collection('chats/DNX9EhYKqZQNftHJCg1L/messages')
+              .snapshots(),
+          builder: (ctx, streamSnapshot) {
+            if (streamSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            final documents = streamSnapshot.data.documents;
+            return ListView.builder(
+              itemCount: streamSnapshot.data.documents.length,
+              itemBuilder: (ctx, index) => Container(
+                child: Text(
+                  documents[index]['text'],
+                ),
+                padding: EdgeInsets.all(8),
+              ),
+            );
+          }),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {
-          Firestore.instance
-              .collection('chats/DNX9EhYKqZQNftHJCg1L/messages')
-              .snapshots()
-              .listen((data) {
-            data.documents.forEach((document) {
-              print(document['text']);
-            });
-          });
-        },
+        onPressed: () {},
       ),
     );
   }
