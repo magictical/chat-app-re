62e348eea7e876b3daf4a8b5e3465d9dc9e9086f
IL KWON KIM <magictical@gmail.com>
2020-07-25 12:21:43 +0900

    fetch message data from firestore

---
+++ lib/chat/messages.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Messages extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Firestore.instance.collection('chat').snapshots(),
+      builder: (ctx, snapShot) {
+        if (snapShot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        final chatDocs = snapShot.data.documents;
+        return ListView.builder(
+          itemCount: chatDocs.length,
+          itemBuilder: (ctx, index) => Text(chatDocs[index]['text']),
+        );
+      },
+    );
+  }
+}
